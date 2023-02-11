00028391b321e19b1cac369f62e4c5f1d03e1225
Luiz Fernando <111098745+luizfernandolpp@users.noreply.github.com>
2023-02-11 02:16:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('img')
-    var hora = new Date().getHours()
-    var título = window.document.getElementById('título')
-    var rodape = window.document.getElementById('rodapé')
-    var seção = window.document.getElementById('section')
+function carregar() {       // Função com evento "onload" -> irá executar sempre que a página for carregada;
+    var msg = window.document.getElementById('msg')   // variável de ligação com a mensagem que informa as horas;
+    var img = window.document.getElementById('img')   // variável de ligação com a imagem a ser escolhida de acordo com a hora; 
+    //var hora = new Date().getHours()                // variável que recebe a hora de acordo com o fuso do sistema do usuário;
+    var hora = 19
+    var título = window.document.getElementById('título')           // variável para modificações no título da página;
+    var rodape = window.document.getElementById('rodapé')           // variável para modificações no rodapé da página;
+    var seção = window.document.getElementById('section')           // variável para modificações na seção da página.
     msg.innerHTML = `Agora são ${hora}h.`
-    if (hora >= 4 && hora < 12) {
+    if (hora >= 4 && hora < 12) {  // condições para o turno da manhã;
         img.src = 'imagens/manhã.png'
-        corpo.style.background = 'linear-gradient(to bottom, var(--alabaster), var(--vanilla), var(--field-drab)'
-        document.body.style.color = 'var(--field-drab)'
-        rodape.style.color = 'var(--field-drab)'
-        seção.style.background = 'var(--alabaster)'
+        document.body.style.background = 'linear-gradient(to bottom, var(--alabaster), var(--vanilla), var(--field-drab)'  // modifica a cor de fundo da página;
+        título.style.color = 'var(--field-drab)'   // modifica a cor da fonte do título da página;
+        rodape.style.color = 'var(--field-drab)'    // modifica a cor da fonte do rodapé da página;
+        seção.style.background = 'var(--alabaster)' // modifica a cor de fundo do conteúdo principal;
     } 
-    else if (hora >= 12 && hora < 18){
+    else if (hora >= 12 && hora < 18){    // condições para o turno da tarde;
         img.src = 'imagens/entardecer.png'
         document.body.style.background = 'linear-gradient(to bottom, var(--black-olive), var(--hunyadi-yellow), var(--chocolate)'
         seção.style.background = 'var(--alabaster)'
     } 
-    else {
+    else {   // condições para o turno da noite;
         img.src = 'imagens/noite.png'
         document.body.style.background = 'linear-gradient(to bottom, var(--midnight-green), var(--hookers-green), black'
         seção.style.background = 'var(--alabaster)'
