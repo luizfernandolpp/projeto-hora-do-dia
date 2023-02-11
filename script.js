e3522729f9ac23c3b7fc283e5eddb29a0e675ec1
Luiz Fernando <111098745+luizfernandolpp@users.noreply.github.com>
2023-02-11 01:52:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,23 @@ function carregar() {
     var hora = new Date().getHours()
     var título = window.document.getElementById('título')
     var rodape = window.document.getElementById('rodapé')
-    var corpo = window.document.body
     var seção = window.document.getElementById('section')
     msg.innerHTML = `Agora são ${hora}h.`
     if (hora >= 4 && hora < 12) {
         img.src = 'imagens/manhã.png'
         corpo.style.background = 'linear-gradient(to bottom, var(--alabaster), var(--vanilla), var(--field-drab)'
-        título.style.color = 'var(--field-drab)'
+        document.body.style.color = 'var(--field-drab)'
         rodape.style.color = 'var(--field-drab)'
         seção.style.background = 'var(--alabaster)'
-
-
-    } else if (hora >= 12 && hora < 18){
+    } 
+    else if (hora >= 12 && hora < 18){
         img.src = 'imagens/entardecer.png'
-        corpo.style.background = 'linear-gradient(to bottom, var(--black-olive), var(--hunyadi-yellow), var(--chocolate)'
+        document.body.style.background = 'linear-gradient(to bottom, var(--black-olive), var(--hunyadi-yellow), var(--chocolate)'
         seção.style.background = 'var(--alabaster)'
-    } else {
+    } 
+    else {
         img.src = 'imagens/noite.png'
-        corpo.style.background = 'linear-gradient(to bottom, var(--midnight-green), var(--hookers-green), black'
+        document.body.style.background = 'linear-gradient(to bottom, var(--midnight-green), var(--hookers-green), black'
         seção.style.background = 'var(--alabaster)'
     }
 }
